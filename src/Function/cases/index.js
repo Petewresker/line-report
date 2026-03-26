3a152092dbbff7a531f29edb5b8b961cfeb5591a
Meammi <bhoriwatn@gmail.com>
2026-03-27 02:32:43 +0700

    POST CASES

--- src/Function/cases/index.js
+++ src/Function/cases/index.js
@@ -1,11 +1,12 @@
 // cases/index.js
-import { getCasesByUser, editCaseHandler } from './handler.js'
+import { getCasesByUser, editCaseHandler, createCase } from './handler.js'
 
 export const handler = async (event) => {
   const { httpMethod, path } = event
 
   if (httpMethod === 'GET' && path === '/cases')       return getCasesByUser(event)
   if (httpMethod === 'POST' && path.includes('/edit')) return editCaseHandler(event)
+  if (httpMethod === 'POST' && path === '/cases') return createCase(event)
 
   return { statusCode: 404, body: 'Not found' }
 }
--- src/Function/cases/service.js
+++ src/Function/cases/service.js
@@ -1,12 +1,27 @@
 // service.js - แตะ DynamoDB
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
 //ใช้คำสั่ง ScanCommand ของ DynamoDB เพื่อดึงข้อมูลทั้งหมดใน Table
-import { DynamoDBDocumentClient, QueryCommand, ScanCommand } from '@aws-sdk/lib-dynamodb'
+import { DynamoDBDocumentClient, QueryCommand, ScanCommand, PutCommand } from '@aws-sdk/lib-dynamodb'
+import crypto from 'node:crypto'
 
-const client = DynamoDBDocumentClient.from(new DynamoDBClient({}))
+// For DyamoDB Local testing
+const clientConfig = {}
+console.log('DYNAMODB_ENDPOINT=', process.env.DYNAMODB_ENDPOINT)
+console.log('TABLE_TABLE_NAME=', process.env.TABLE_TABLE_NAME)
+
+if (process.env.DYNAMODB_ENDPOINT) {
+  clientConfig.endpoint = process.env.DYNAMODB_ENDPOINT
+  clientConfig.region = 'us-east-1'
+  clientConfig.credentials = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'local',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'local',
+  }
+}
+
+const client = DynamoDBDocumentClient.from(new DynamoDBClient(clientConfig))
 
 export const editCaseService = async (caseId, data) => {
-  // update DynamoDB ตรงนี้
+  
 }
 
 export const getCasesByUserService = async (userId) => {
@@ -24,4 +39,31 @@ export const getAllCasesService = async () => {
     TableName: process.env.TABLE_TABLE_NAME,
   }))
   return result.Items
+}
+
+export const createCaseService = async (caseInformation) => {
+  const caseId = crypto.randomUUID()
+  const now = new Date().toISOString()
+
+  const item = {
+    PK: `CASE#${caseId}`,
+    SK: 'METADATA',
+    caseId,
+    title: caseInformation.title,
+    description: caseInformation.description,
+    userId: caseInformation.userId,
+    lat: caseInformation.lat,
+    lon: caseInformation.lon,
+    imageUrlBefore: caseInformation.imageUrlBefore,
+    status: 'PENDING',
+    createdAt: now,
+    updatedAt: now,
+  }
+
+  await client.send(new PutCommand({
+    TableName: process.env.TABLE_TABLE_NAME,
+    Item: item,
+  }))
+  
+  return item
 }
