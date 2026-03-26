83b7a441904cd39895b6e2b7ed7bea51c84786bf
Meammi <bhoriwatn@gmail.com>
2026-03-27 02:32:43 +0700

    POST CASES

--- src/Function/cases/handler.js
+++ src/Function/cases/handler.js
@@ -1,5 +1,5 @@
 // handler.js - validate แล้วเรียก service
-import { editCaseService, getCasesByUserService, getAllCasesService } from './service.js'
+import { editCaseService, getCasesByUserService, getAllCasesService, createCaseService } from './service.js'
 export const editCaseHandler = async (event) => {
   const { caseId } = event.pathParameters
   const body = JSON.parse(event.body)
@@ -16,4 +16,11 @@ export const getCasesByUser = async (event) => {
     : await getAllCasesService() 
     
   return { statusCode: 200, body: JSON.stringify(result) }
+}
+
+export const createCase = async (event) => {
+  const body = JSON.parse(event.body)
+
+  const result = await createCaseService(body)
+  return { statusCode: 201, body: JSON.stringify(result) }
 }
