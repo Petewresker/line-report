bedc0fe368e9c5c23398840cb78d90969083554c
Petewresker <minecopper.55@gmail.com>
2026-03-27 10:38:49 +0700

    Testing mock case sender

---
+++ src/Function/cases/mockData.js
@@ -0,0 +1,24 @@
+
+export const mockData = [
+  {
+    userId: "Ue714016cf2d1125a2bb009c88ebe878c",
+    Topic: "Fire",
+    description: "Fire is everywhere",
+    location: { lat: 13.8161, long: 100.5615 },
+    imageUrlBefore: "https://incident-line-tu.s3.us-east-1.amazonaws.com/cases/case_1.jpg",
+  },
+  {
+    userId: "Ue714016cf2d1125a2bb009c88ebe878c",
+    Topic: "Traffic",
+    description: "Tarffic is everywhere",
+    location: { lat: 13.8161, long: 100.5615 },
+    imageUrlBefore: "https://incident-line-tu.s3.us-east-1.amazonaws.com/cases/case_2.jpg",
+  },
+  {
+    userId: "Ue714016cf2d1125a2bb009c88ebe878c",
+    Topic: "Danger",
+    description: "Danger is everywhere!",
+    location: { lat: 13.8161, long: 100.5615 },
+    imageUrlBefore: "https://incident-line-tu.s3.us-east-1.amazonaws.com/cases/case_3.jpg",
+  },
+];
