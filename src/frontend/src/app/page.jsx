f6041f210536c68723580a8cfdfd6a521335c13e
PeraphatEkdit <peraphat.ekd@dome.tu.ac.th>
2026-04-11 13:59:12 +0700

    Fixing problem

--- src/frontend/src/app/page.jsx
+++ src/frontend/src/app/page.jsx
@@ -261,7 +261,7 @@ export default function Home() {
                 <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
                 <circle cx="12" cy="13" r="4" />
               </svg>
-              <span className="text-gray-400 text-sm">Please take a photo</span>
+              <span className="text-gray-400 text-sm">Please</span>
             </div>
           </div>
         )}
