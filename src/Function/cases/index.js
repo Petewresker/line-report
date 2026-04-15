9e823a472678aa36fff058291efa51e8bdc3ebcb
PeraphatEkdit <peraphat.ekd@dome.tu.ac.th>
2026-04-15 15:34:06 +0700

    Line local storage

--- src/Function/cases/index.js
+++ src/Function/cases/index.js
@@ -11,25 +11,24 @@ export const handler = async (event) => {
   try {
     const { httpMethod, path, resource } = event
 
+    //API เทสระบบว่ามีปลายทางไหม
     if (httpMethod === 'OPTIONS') return { statusCode: 200, headers: CORS_HEADERS, body: '' }
 
-    //==================================== GET ====================================
-    if (httpMethod === 'GET' && resource === '/cases/presigned-url') return getPresignedUrl(event)
-    if (httpMethod === 'GET' && resource === '/cases')       return getCasesByUser(event)
-    if (httpMethod === 'GET' && resource === '/cases/hotspots') return gethotspot()
-    if (httpMethod === 'GET' && resource === '/cases/trends') return getTrends()
-    if (httpMethod === 'GET' && resource === '/cases/resolution') return getResolution()
-    if (httpMethod === 'GET' && resource === '/cases/monthly') return getMonthlyReport()
+    if (httpMethod === 'GET' && path === '/cases/presigned-url') return getPresignedUrl(event)
+    if (httpMethod === 'GET' && path === '/cases')       return getCasesByUser(event)
+    if (httpMethod === 'POST' && path.includes('/edit')) return editCaseHandler(event)
+    if (httpMethod === 'POST' && path === '/cases') return createCase(event)
+    if (httpMethod === 'DELETE' && path === '/cases') return deleteCase(event)
 
-    //==================================== POST ====================================  
-    if (httpMethod === 'POST' && resource === '/cases/{caseId}/edit') return editCaseHandler(event)
-    if (httpMethod === 'POST' && resource === '/cases') return createCase(event)
-    if (httpMethod === 'POST' && resource === '/cases/mockpost') return seedMockCases()
+    //สําหรับการวิเคราะห์ข้อมูล
+    if (httpMethod === 'GET' && path === '/cases/hotspots') return gethotspot()
+    if (httpMethod === 'GET' && path === '/cases/trends') return getTrends()
+    if (httpMethod === 'GET' && path === '/cases/resolution') return getResolution()
+    if (httpMethod === 'GET' && path === '/cases/monthly') return getMonthlyReport()
 
-    //==================================== DELETE ====================================
-    if (httpMethod === 'DELETE' && resource === '/cases/all') return deleteAllCases()
-    if (httpMethod === 'DELETE' && resource === '/cases') return deleteCase(event)
-    if (httpMethod === 'DELETE' && resource === '/cases/{caseId}') {return deleteByCaseId(event)}
+    //วิเคราะห์ข้อมูล
+    if (httpMethod === 'DELETE' && path === '/cases/all') return deleteAllCases()
+    if (httpMethod === 'POST' && path === '/cases/mockpost') return seedMockCases()
 
     return {
       statusCode: 404,
