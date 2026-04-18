3e62e59c706c6044c7a0208662f4ad2ab4835a8d
Panupong Suttikasem <panupong.suttikasem@gmail.com>
2026-04-18 23:07:11 +0700

    Add: add condition for admin role

--- src/Function/auth/service.js
+++ src/Function/auth/service.js
@@ -64,6 +64,10 @@ export async function findOrCreateUser(lineUserId, lineProfile) {
         userData.agencyId = Item.agencyId;
       }
       
+      if (Item.adminId) {
+        userData.adminId = Item.adminId;
+      }
+      
       return userData;
     }
   
